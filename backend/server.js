ffab5265f9505cba1e2d4870dbe42c9720c7f30d
niyatidave2112@gmail.com <niyatidave2112@gmail.com>
2024-11-16 13:03:14 +0530

    committed changes in server.js

--- backend/server.js
+++ backend/server.js
@@ -2,5 +2,5 @@ const app = require('./app');
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(Server is running on port ${PORT});
+  console.log(`Server is running on port ${PORT}`);
 });
